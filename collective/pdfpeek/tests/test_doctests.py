ae1c1cddad371270867fe8e657d415424687845d
Georg Gogo. BERNHARD <gogo@bluedynamics.com>
2018-06-06 14:27:22 +0200

    Mostly PEP8
    Trying to get through travis ci

--- collective/pdfpeek/tests/test_doctests.py
+++ collective/pdfpeek/tests/test_doctests.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+"""Fix other people's missing docstrings."""
+
 from collective.pdfpeek import testing
 from plone.testing import layered
 import doctest
@@ -12,13 +14,13 @@ functional_tests = [
 
 
 def test_suite():
+    """Fix other people's missing docstrings."""
     return unittest.TestSuite(
         [layered(doctest.DocFileSuite('tests/{0:s}'.format(f),
                                       package='collective.pdfpeek',
                                       optionflags=testing.optionflags),
                  layer=testing.PDFPEEK_AT_INTEGRATION_TESTING)
-            for f in integration_tests]
-        +
+            for f in integration_tests] +
         [layered(doctest.DocFileSuite('tests/{0:s}'.format(f),
                                       package='collective.pdfpeek',
                                       optionflags=testing.optionflags),
